bdd2ac2f6c64ceb232f96c47393d0f5c1dcdfece
federicobenitezrk <federico.benitez@aurea.com>
2022-07-24 07:38:48 -0300

    Initialized CDK

---
+++ test/cdk-multiple-environments.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkMultipleEnvironments from '../lib/cdk-multiple-environments-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-multiple-environments-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkMultipleEnvironments.CdkMultipleEnvironmentsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
